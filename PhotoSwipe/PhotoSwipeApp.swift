8e827d0cb8ea8c7463012a5ac4a4a7a156492b2a
Alessio Novel <alessionovel@gmail.com>
2026-07-17 12:30:39 +0200

    Initial Commit

---
+++ PhotoSwipe/PhotoSwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoSwipeApp.swift
+//  PhotoSwipe
+//
+//  Created by Alessio Novel on 17/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct PhotoSwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
